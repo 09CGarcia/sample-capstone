0878944a758efb8862bd4f1a3a684f72e442b7c5
maestraTin <09christine.garcia@gmail.com>
2021-12-17 14:06:08 +0800

    devOps capstone

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 4001;
+
+let app = express();
+
+let delivery = '';
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	console.log(delivery)
+	res.status(200).send(delivery)
+});
+
+app.post('/result', (req, res) => {
+	delivery = req.body
+	res.send(delivery)
+});
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+
