f9921585bbbc3e2ea2e7bbd5d3cba511f8922c72
maestraTin <09christine.garcia@gmail.com>
2021-12-17 14:52:24 +0800

    added: changes

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const PORT = process.env.PORT || 4001;
 let app = express();
 
 let delivery = '';
-
+let deliverycache = []
 app.use(express.json());
 
 app.get('/', (req, res) => {
-	console.log(delivery)
+	//console.log(deliverycache)
 	res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.status(200).send(delivery)
+	res.status(200).send(deliverycache)
 });
 
 app.post('/result', (req, res) => {
-	delivery += req.body
-	//console.log(delivery)
+	delivery = req.body
+	deliverycache.push(delivery)
+	console.log(deliverycache)
 	res.send(delivery)
 });
 
